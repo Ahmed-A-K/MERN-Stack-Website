e634befbf660291f10e877948f28ecd18f0d4e0d
Ahmed-A-K <ahmedalikhan652@gmail.com>
2023-01-10 19:16:20 -0800

    Built the User list component
    Completed the user list component with the final styling. Also added Link which will dynamically route the the specific user ID to its place. Finally learned difference between statefull  and presentational components.

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UserList from "../components/UserList";
 
 const Users = () => {
-  return <h1>User is here</h1>;
+  const USERS = [
+    {
+      id: "u1",
+      image: "https://picsum.photos/id/237/200/300",
+      name: "john Doe",
+      places: 4,
+    },
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
