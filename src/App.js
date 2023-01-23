db5e83c58285f582f9268fb95c115e5c3cec6e82
Ahmed-A-K <ahmedalikhan652@gmail.com>
2023-01-22 18:31:17 -0800

    22/01/2023
    completed side drawer and main navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Users />} />
-        <Route path='/places/new' element={<NewPlace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path='/' element={<Users />} />
+          <Route path='/:userId/places' element={<UserPlaces />} />
+          <Route path='/places/new' element={<NewPlace />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
